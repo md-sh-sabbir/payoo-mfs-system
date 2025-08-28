e1590089b08cf20a4403fb78fb1127978918d5bf
md-sh-sabbir <sabbirhossain9870@gmail.com>
2025-08-28 22:41:01 +0600

    reusable function implimented in javascript file

--- assests/js/home.js
+++ assests/js/home.js
@@ -1,5 +1,14 @@
 const validPin = 1234
 
+// function to get input values
+function getInputValueNumber(id){
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    const inputFieldValueNumber = parseInt(inputFieldValue)
+
+    return inputFieldValueNumber
+}
+
 // For Add money operation in Home Page
 document.getElementById('add-money-btn').addEventListener('click', function(e){
     e.preventDefault()
@@ -7,8 +16,8 @@ document.getElementById('add-money-btn').addEventListener('click', function(e){
 
     const bank = document.getElementById('bank').value 
     const accountNumber = document.getElementById('account-number').value 
-    const amount = parseInt(document.getElementById('add-amount').value) 
-    const pin = parseInt(document.getElementById('add-pin').value)
+    const amount = getInputValueNumber('add-amount')
+    const pin = getInputValueNumber('add-pin')
     const availableBalance = parseInt(document.getElementById('available-balance').innerText)
 
     console.log(availableBalance)
@@ -34,8 +43,8 @@ document.getElementById('withdraw-money-btn').addEventListener('click', function
     console.log('Withdraw button clicked')
 
     const agentNumber = document.getElementById('agent-number').value
-    const withdrawAmount = document.getElementById('withdraw-amount').value
-    const pin = parseInt(document.getElementById('withdraw-pin').value)
+    const withdrawAmount = getInputValueNumber('withdraw-amount')
+    const pin = getInputValueNumber('withdraw-pin')
     const availableBalance = parseInt(document.getElementById('available-balance').innerText)
 
     console.log(availableBalance)
